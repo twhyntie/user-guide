d499f9932c7c88d0ace78d2b28335bdaee891266
Tom Whyntie <Tom Whyntie>
2015-08-03 13:50:23 +0100

    Added achievements and adjusted small breakpoint.

--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ exports = module.exports = function(app) {
   // Front end.
   app.get('/', require('./views/index').init);
   app.get('/about/', require('./views/about/index').init);
+  app.get('/achievements/', require('./views/achievements/index').init);
 
 }
