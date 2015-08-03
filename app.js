fdc9cf90db2a178f92b43a27a3665ea459106b9f
Tom Whyntie <Tom Whyntie>
2015-08-03 12:29:48 +0100

    Removed ref to routes/index.js etc.

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 app.config = config;
